8370bc70299bcf8037704241802d22fd75b023ae
taha <159454958+ahmettahaondes@users.noreply.github.com>
2026-07-04 19:25:09 +0300

    Add system information bash script

---
+++ system-info.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "===== SYSTEM INFORMATION ====="
+echo "Hostname : $(hostname)"
+echo "User     : $(whoami)"
+echo "Date     : $(date)"
+echo "Kernel   : $(uname -r)"
+echo "Uptime   : $(uptime)"
